f21693820f718ebbc263156d1a18caf8e2f3a14c
Donghyun Kim <dong32@gmail.com>
2020-06-05 11:40:00 +0900

    add xlmroberta

--- XLMRoberta/run_nsml.sh
+++ XLMRoberta/run_nsml.sh
@@ -6,14 +6,14 @@ nsml run \
   -g 1 \
   -c 1 \
   -e run_squad.py \
-  -a "--model_type electra
-    --model_name_or_path monologg/koelectra-base-v2-generator
+  -a "--model_type xlmroberta
+    --model_name_or_path xlm-roberta-large
     --do_train
     --do_eval
     --data_dir train
     --num_train_epochs 10
-    --per_gpu_train_batch_size 24
-    --per_gpu_eval_batch_size 24
+    --per_gpu_train_batch_size 8
+    --per_gpu_eval_batch_size 8
     --output_dir output
     --overwrite_output_dir
     --logging_steps 300
