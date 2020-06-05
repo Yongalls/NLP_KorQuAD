d0720e7a96547c0247ea8ae6e8fbb89104087a67
Donghyun Kim <dong32@gmail.com>
2020-06-05 09:43:50 +0900

    clean up

--- KOELECTRA/transformers/configuration_bert.py
+++ KOELECTRA/transformers/configuration_bert.py
@@ -46,6 +46,7 @@ BERT_PRETRAINED_CONFIG_ARCHIVE_MAP = {
     "bert-base-finnish-cased-v1": "https://s3.amazonaws.com/models.huggingface.co/bert/TurkuNLP/bert-base-finnish-cased-v1/config.json",
     "bert-base-finnish-uncased-v1": "https://s3.amazonaws.com/models.huggingface.co/bert/TurkuNLP/bert-base-finnish-uncased-v1/config.json",
     "bert-base-dutch-cased": "https://s3.amazonaws.com/models.huggingface.co/bert/wietsedv/bert-base-dutch-cased/config.json",
+    "google/electra-base-generator": "https://s3.amazonaws.com/models.huggingface.co/bert/google/electra-base-generator/config.json",
 }
 
 
