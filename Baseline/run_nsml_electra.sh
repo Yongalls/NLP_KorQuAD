394a406da4ff13193eff7e428d23e67adb189773
Donghyun Kim <dong32@gmail.com>
2020-06-21 17:00:30 +0900

    Baseline, EDA, SD uploaded, codes cleaned up

---
+++ Baseline/run_nsml_electra.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+nsml run \
+  -m 'kaist korquad open' \
+  -d korquad-open-ldbd \
+  -g 1 \
+  -c 1 \
+  -e run_squad.py \
+  -a "--model_type electra
+    --model_name_or_path monologg/koelectra-base-v2-discriminator
+    --do_train
+    --do_eval
+    --data_dir train
+    --num_train_epochs 10
+    --per_gpu_train_batch_size 24
+    --per_gpu_eval_batch_size 24
+    --output_dir output
+    --overwrite_output_dir
+    --logging_steps 500
+    --version_2_with_negative
+    --dropout 0.1
+    --start_loss
+    --answer_loss
+    --learning_rate 1e-5
+    "
---
+++ Self_Distillation/SDA/run_nsml_bert.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+nsml run \
+  -m 'kaist korquad open' \
+  -d korquad-open-ldbd \
+  -g 1 \
+  -c 1 \
+  -e run_squad.py \
+  -a "--model_type bert
+    --model_name_or_path bert-base-multilingual-cased
+    --do_train
+    --do_eval
+    --data_dir train
+    --num_train_epochs 10
+    --per_gpu_train_batch_size 4
+    --per_gpu_eval_batch_size 4
+    --output_dir output
+    --overwrite_output_dir
+    --logging_steps 100
+    --version_2_with_negative
+    --dropout 0.1
+    --K 5
+    --gradient_accumulation_steps 4
+    "
